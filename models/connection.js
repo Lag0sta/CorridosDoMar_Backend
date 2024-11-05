a7a7434fadb70f9c047287532bcfe2eea57a3676
LRonnet <leo.ronnet@gmail.com>
2024-11-05 13:19:54 +0100

    secure

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://llagosta:5EFxj7cB3I8xj8Mx@cluster0.wninfmo.mongodb.net/corridosDM';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
 	.then(() => console.log('Data Connected'))
